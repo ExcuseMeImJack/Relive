cd64812dd6e77c0eb5a22a81380d15771eb4205c
Jack Roybal <excusemeimjack@gmail.com>
2023-03-22 21:38:02 -0700

    Local create-a-review-for-spot-id working

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -24,6 +24,23 @@ const doesSpotExist = async (spotIdParam) => {
   else return true;
 };
 
+const doesReviewExist = async (userId, spotId) => {
+  const reviews = await Review.findAll({ where: {spotId: spotId}, attributes: ["userId"] });
+  let reviewsList = [];
+  let userIds = [];
+
+  reviews.forEach((review) => {
+    reviewsList.push(review.toJSON());
+  });
+
+  reviewsList.forEach((review) => {
+    userIds.push(review.userId);
+  });
+
+  if (userIds.includes(userId)) return false;
+  else return true;
+};
+
 const validateSpot = [
   check("address")
     .exists({ checkFalsy: true })
@@ -52,6 +69,38 @@ const validateSpot = [
   handleValidationErrors,
 ];
 
+const validateReview = [
+  check('review').exists({checkFalsy: true}).withMessage("Review text is required"),
+  check('stars').isInt({min: 1, max: 5}).withMessage("Stars must be an integer from 1 to 5"),
+  handleValidationErrors
+]
+
+// Create a Review for a Spot based on the Spot's id
+router.post('/:spotId/reviews', [requireAuth, validateReview], async (req, res) => {
+  const {user} = req;
+
+  if ((await doesSpotExist(req.params.spotId)) === false)
+  return res.status(404).json({ message: "Spot couldn't be found" });
+
+  if(user) {
+    if ((await doesReviewExist(user.id, req.params.spotId)) === false) return res.status(403).json({ message: "User already has a review for this spot" });
+  }
+
+  if(user){
+    const spotIdInt = parseInt(req.params.spotId)
+    const {review, stars} = req.body;
+    const newReview = await Review.create({
+      userId: user.id,
+      spotId: spotIdInt,
+      review,
+      stars
+    })
+
+    res.status(201).json(newReview);
+  }
+});
+
+// Get all Reviews by a Spot's id
 router.get('/:spotId/reviews', async (req, res) => {
 
   if ((await doesSpotExist(req.params.spotId)) === false)
